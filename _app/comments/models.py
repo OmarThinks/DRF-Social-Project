3991b41eb010308113a5eedd26b63560554bb6e4
Omar Magdy <contact.parmag@gmail.com>
2021-06-20 16:34:30 +0200

    created the comments model

--- _app/comments/models.py
+++ _app/comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+	post = models.ForeignKey(Post,
+		on_delete=models.CASCADE)
+	content = models.CharField(max_length=1000) 
+	author = models.ForeignKey(settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE)
+
+	def __str__(self):
+		return (str(self.id) + " By "+ self.author.username 
+			+ " on post"+ str(self.post_id) +") " + self.content) 
