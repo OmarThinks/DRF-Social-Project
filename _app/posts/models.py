33ddd10fead8f7aaee24b618941951dc466ba5f4
Omar Magdy <contact.parmag@gmail.com>
2021-06-20 14:58:29 +0200

    Post model: created

--- _app/posts/models.py
+++ _app/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+	content = models.CharField(max_length=1000) 
+	author = models.ForeignKey(settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE)
