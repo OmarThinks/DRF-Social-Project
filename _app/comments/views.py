62dc2e5a8e6f110a7edda52ef4f87e88b7ae5566
Omar Magdy <contact.parmag@gmail.com>
2021-06-20 16:38:07 +0200

    comments:views:created

--- _app/comments/views.py
+++ _app/comments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Comment
+from .serializers import CommentSerializer
+
 
 # Create your views here.
+# ViewSets define the view behavior.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
