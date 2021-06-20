394593ad526897d4226a7f07aadde63388c1a3d9
Omar Magdy <contact.parmag@gmail.com>
2021-06-20 14:38:53 +0200

    implemented djoser auth

--- _app/_app/urls.py
+++ _app/_app/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/auth/', include('djoser.urls')),
+    path('api-auth/auth/', include('djoser.urls.jwt')),
 ]
